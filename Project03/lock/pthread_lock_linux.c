6fae944214d04506439d69ba5a3ac1dad2b81d79
my3ngd <hblee0402@gmail.com>
2024-05-14 14:44:13 +0900

    project3: lock (template)

--- Project03/lock/pthread_lock_linux.c
+++ Project03/lock/pthread_lock_linux.c
@@ -11,34 +11,49 @@ void unlock();
 
 void lock()
 {
+    ;
 }
 
 void unlock()
 {
+    ;
 }
 
-void* thread_func(void* arg) {
+
+int increase_with_delay(int x)
+{
+    for (int i = 0, delay_value = 0; i < 1000; i++)
+        delay_value += i;
+    return ++x;
+}
+
+
+void* thread_func(void* arg)
+{
     int tid = *(int*)arg;
-    
     lock();
-    
-        for(int i = 0; i < NUM_ITERS; i++)    shared_resource++;
-    
+
+    for (int i = 0; i < NUM_ITERS; i++)
+        shared_resource = increase_with_delay(shared_resource);
+
     unlock();
-    
     pthread_exit(NULL);
 }
 
-int main() {
-    pthread_t threads[n];
-    int tids[n];
+
+int main()
+{
+    pthread_t threads[NUM_THREADS];
+    int tids[NUM_THREADS];
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
         tids[i] = i;
         pthread_create(&threads[i], NULL, thread_func, &tids[i]);
     }
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < NUM_THREADS; i++)
+    {
         pthread_join(threads[i], NULL);
     }
 
---
+++ Project03/lock/run.sh
@@ -0,0 +1,3 @@
+rm pthread_lock_linux
+gcc pthread_lock_linux.c -o pthread_lock_linux -lpthread
+./pthread_lock_linux
